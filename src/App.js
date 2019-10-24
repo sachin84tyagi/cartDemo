93e356d7e6543ed76decd9fe1edbe449fd88aef6
Sachin Tyagi <SachinTy@hcl.com>
2019-10-24 16:32:43 +0530

    first commit push code

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Header from "./shared/header/header";
+import Navigation from "./shared/navigation/navigation";
+import Footer from "./shared/footer/footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Header>
+
+      </Header>
+
+      <Navigation>
+
+      </Navigation>
+      Some Content
+<Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
